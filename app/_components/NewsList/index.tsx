4a4cb75ef74c96fae5edf35168121b25d0593a9e
Ryosuke Moroe <56299430+moroq1988@users.noreply.github.com>
2024-11-21 08:16:55 +0900

    Complete Section 3-3

---
+++ app/_components/NewsList/index.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import Image from "next/image";
+import { News } from "@/app/_libs/microcms";
+import styles from "./index.module.css";
+import Category from "../Category";
+import Date from "../Date";
+
+type Props = {
+  news: News[];
+};
+
+export default function NewsList({ news }: Props) {
+  if (news.length === 0) {
+    return <p>記事がありません。</p>;
+  }
+
+  return (
+    <ul>
+      {news.map((article) => (
+        <li key={article.id} className={styles.list}>
+          <div className={styles.link}>
+            <Image
+              className={styles.image}
+              src="/no-image.png"
+              alt="No Image"
+              width={1200}
+              height={630}
+              priority
+            />
+            <dl className={styles.content}>
+              <dt className={styles.title}>{article.title}</dt>
+              <dd className={styles.meta}>
+                <Category category={article.category} />
+                <Date date={article.publishedAt ?? article.createdAt} />
+              </dd>
+            </dl>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+}
