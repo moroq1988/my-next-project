8861eb4bcde313f96d82b073f52e704c2a906f9c
Ryosuke Moroe <56299430+moroq1988@users.noreply.github.com>
2024-11-30 21:38:11 +0900

    Complete Section 5-3

--- app/_libs/microcms.ts
+++ app/_libs/microcms.ts
@@ -1,3 +1,10 @@
+import {
+  createClient,
+  MicroCMSImage,
+  MicroCMSListContent,
+  MicroCMSQueries,
+} from "microcms-js-sdk";
+
 export type Category = {
   name: string;
 };
@@ -10,4 +17,32 @@ export type News = {
   };
   publishedAt: string;
   createdAt: string;
+};
+
+export type Member = {
+  name: string;
+  position: string;
+  profile: string;
+  image: MicroCMSImage;
+} & MicroCMSListContent;
+
+if (!process.env.MICROCMS_SERVICE_DOMAIN) {
+  throw new Error("MICROCMS_SERVICE_DOMAIN is required");
+}
+
+if (!process.env.MICROCMS_API_KEY) {
+  throw new Error("MICROCMS_API_KEY is required");
+}
+
+const client = createClient({
+  serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN,
+  apiKey: process.env.MICROCMS_API_KEY,
+});
+
+export const getMembersList = async (queries?: MicroCMSQueries) => {
+  const listData = await client.getList<Member>({
+    endpoint: "members",
+    queries,
+  });
+  return listData;
 };
