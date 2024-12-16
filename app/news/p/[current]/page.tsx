03a29012903d44a516416dcb462de69d6789149d
Ryosuke Moroe <56299430+moroq1988@users.noreply.github.com>
2024-12-17 08:00:35 +0900

    Complete Section 7-2

---
+++ app/news/p/[current]/page.tsx
@@ -0,0 +1,36 @@
+import NewsList from "@/app/_components/NewsList";
+import Pagination from "@/app/_components/Pagination";
+import { NEWS_LIST_LIMIT } from "@/app/_constants";
+import { getNewsList } from "@/app/_libs/microcms";
+import { notFound } from "next/navigation";
+import React from "react";
+
+type Props = {
+  params: {
+    current: string;
+  };
+};
+
+export default async function Page({ params }: Props) {
+  const current = parseInt(params.current, 10);
+
+  if (Number.isNaN(current) || current < 1) {
+    notFound();
+  }
+
+  const { contents: news, totalCount } = await getNewsList({
+    offset: NEWS_LIST_LIMIT * (current - 1),
+    limit: NEWS_LIST_LIMIT,
+  });
+
+  if (news.length === 0) {
+    notFound();
+  }
+
+  return (
+    <>
+      <NewsList news={news} />
+      <Pagination totalCount={totalCount} current={current} />
+    </>
+  );
+}
--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,8 +1,17 @@
 import React from "react";
 import { getNewsList } from "../_libs/microcms";
 import NewsList from "../_components/NewsList";
+import Pagination from "../_components/Pagination";
+import { NEWS_LIST_LIMIT } from "../_constants";
 
 export default async function Page() {
-  const { contents: news } = await getNewsList();
-  return <NewsList news={news} />;
+  const { contents: news, totalCount } = await getNewsList({
+    limit: NEWS_LIST_LIMIT,
+  });
+  return (
+    <>
+      <NewsList news={news} />
+      <Pagination totalCount={totalCount} />
+    </>
+  );
 }
